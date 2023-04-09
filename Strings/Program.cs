8cca3adfebcd6b89f08fb2aa19e5bc29e1aa82dd
Renzo Herrera Giuria <Renzo Herrera Giuria@DESKTOP-FPEFDKS>
2023-04-08 21:45:32 -0500

    Agregar archivos de proyecto.

---
+++ Strings/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Los strings SIEMPRE van en comillas
+
+Console.WriteLine("Ricardo Perez ID!");
+int altura = 168;
+int edad = 89;
+string nombre = "Ricardo Perez";
+string informacion = "Nació en Hidalgo, es estudiante de gastronomía y astronomía";
+var hobby = "deportista";
+
+
+string tarjetadeidentificacion = $"La información de {nombre} es la siguiente, \n Su edad es {edad} años \n " +
+    $"su altura es {altura} cm \n Información relevante {informacion} \n Su hobby es {hobby}";
+
+Console.WriteLine(tarjetadeidentificacion);
